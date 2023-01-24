0ad8c04f0ecd46723d66fc4a9f6853fb22fb7013
Tom Petr <petrt@rpi.edu>
2023-01-23 23:59:02 -0500

    functioning with pthreads

--- Project1/compress_multithread.cpp
+++ Project1/compress_multithread.cpp
@@ -21,15 +21,17 @@ Authors: Ben Haft, Thomas Petr
 #define SEGMENT_LENGTH 16000
 
 typedef struct compress_args {
+    char* cBlock;
     char* cOut;
     size_t outdata_length;
 } compress_args_t;
 
-compress_args_t* compressHelper(const char* cBlock)
+void* compressHelper(void* args)
 {
-    size_t const buffInSize = strlen(cBlock);
+    compress_args_t* cargs = (compress_args_t*)args;
+    size_t const buffInSize = strlen(cargs->cBlock);
     size_t const buffOutSize = ZSTD_compressBound(buffInSize);
-    void* const buffOut = malloc_orDie(buffOutSize);
+    cargs->cOut = (char*)malloc_orDie(buffOutSize);
 
 
     ZSTD_CStream* const cstream = ZSTD_createCStream();
@@ -41,9 +43,9 @@ compress_args_t* compressHelper(const char* cBlock)
         exit(11);
     }
 
-    ZSTD_inBuffer input = { cBlock, buffInSize, 0 };
+    ZSTD_inBuffer input = { cargs->cBlock, buffInSize, 0 };
     
-    ZSTD_outBuffer output = { buffOut, buffOutSize, 0 };
+    ZSTD_outBuffer output = { cargs->cOut, buffOutSize, 0 };
     int chk=0;
     chk = ZSTD_compressStream(cstream, &output , &input);  
     if (ZSTD_isError(chk)) {
@@ -55,13 +57,12 @@ compress_args_t* compressHelper(const char* cBlock)
     size_t const remainingToFlush = ZSTD_endStream(cstream, &output);   /* close frame */
     if (remainingToFlush) { fprintf(stderr, "not fully flushed"); exit(13); }
 
-    compress_args_t* args;
-    args->cOut = (char*)buffOut;
-    args->outdata_length = output.pos;
+    // cargs->cOut = (char*)buffOut;
+    cargs->outdata_length = output.pos;
 
     ZSTD_freeCStream(cstream);
-
-    return args;
+    pthread_exit(NULL);
+    // return NULL;
 }
 
 
@@ -86,17 +87,7 @@ static void compressFile(const char* inName, const char* outName, int num_thread
 
     // Find number of 16kB segments, round up
     int num_segments = ceil((float)length / (float)SEGMENT_LENGTH);
-
-    // Read data in by 16kB chunks -> array of 16kB C strings
-    char** input_data = new char*[num_segments];
-    for(int i = 0; i < num_segments; i++)
-    {
-        char* buffer = new char[SEGMENT_LENGTH];
-        infile.read(buffer, SEGMENT_LENGTH);
-
-        input_data[i] = buffer;
-    }
-
+    
     // Determine if number of threads input is more or less than num segments
     if(num_segments <= num_threads){
         num_threads = num_segments;
@@ -104,22 +95,23 @@ static void compressFile(const char* inName, const char* outName, int num_thread
         pthread_t threads[num_threads];
         int rc;
 
-        // compress_args_t **all_output = new compress_args_t*[num_threads];
+        compress_args_t **all_output = new compress_args_t*[num_threads];
         for(int i = 0; i < num_threads; i++){
-            // all_output[i] = (compress_args_t*)malloc_orDie(sizeof(compress_args_t*));
-            // all_output[i]->cBlock = input_data[i];
+            compress_args_t* cargs = new compress_args_t;
+            cargs->cBlock = new char[SEGMENT_LENGTH];
 
-            compress_args_t* cargs = compressHelper2(input_data[i]);
-            // printf("%s\n", cOut);
-            // rc = pthread_create(&threads[i], NULL, compressHelper, all_output[i]);
-            fwrite_orDie(cargs->cOut, cargs->outdata_length, fout);
+            // Read data in by 16kB chunks -> array of 16kB C strings
+            infile.read(cargs->cBlock, SEGMENT_LENGTH);
+            all_output[i] = cargs;
+            
+            rc = pthread_create(&threads[i], NULL, compressHelper, all_output[i]);
         }
 
-        // for(int i = 0; i < num_threads; i++){
-        //     // pthread_join(threads[i], NULL);
+        for(int i = 0; i < num_threads; i++){
+            pthread_join(threads[i], NULL);
             
-        //     fwrite_orDie(all_output[i]->cOut, all_output[i]->outdata_length, fout);
-        // }
+            fwrite_orDie(all_output[i]->cOut, all_output[i]->outdata_length, fout);
+        }
     }
     else{
         pthread_t threads[num_threads];
@@ -161,7 +153,6 @@ static char* createOutFilename(const char* filename)
 int main(int argc, const char** argv)
 {
     const char* const exeName = argv[0]; // Name of file to compress
-
 
     if (argc==2) { // Need two runtime input arguements
         printf("Wrong Arguments\n");
