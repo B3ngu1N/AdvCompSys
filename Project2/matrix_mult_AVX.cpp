0cef59987bbbf987716031b819152bf52865b7b4
Tom Petr <petrt@rpi.edu>
2023-02-10 15:57:45 -0500

    initial code for 16b int

--- Project2/matrix_mult_AVX.cpp
+++ Project2/matrix_mult_AVX.cpp
@@ -1,7 +1,7 @@
 /*
   This program uses AVX SIMD functions to parallelize a matrix-matrix multiplication.
 
-  Compilation: g++ matrix_mult.cpp -mavx -mfma -o mult_avx.o
+  Compilation: g++ matrix_mult.cpp -mavx -msse -o mult_avx.o
 
   @author Thomas Petr
   @author Ben Haft
@@ -12,8 +12,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
+#include <string>
 #include <bits/stdc++.h>
 #include <immintrin.h>
+#include <emmintrin.h>
 
 
 template <typename T>
@@ -22,7 +24,10 @@ class Matrix {
  // Need to add padding to allocation to allow for 0s at end of rows and columns
  // to be a multiple of 256 bits
   Matrix(int matrix_dim) : rows_(matrix_dim), cols_(matrix_dim) {
-    int segment_breakup = 256/(sizeof(T)*8);
+    // choose SSE or AVX depending on input type
+    int avx_or_sse = 256;
+    if(sizeof(T) < 4) avx_or_sse = 128;
+    int segment_breakup = avx_or_sse/(sizeof(T)*8);
     int padding_dim = ceil((float)matrix_dim/segment_breakup) * segment_breakup;
     data_ = new T[padding_dim * padding_dim];
     memset(data_, (T)0.0, sizeof(T) * padding_dim * padding_dim);
@@ -72,6 +77,20 @@ void Matrix<float>::printMatrix()
 }
 
 template <>
+void Matrix<short>::printMatrix()
+{
+  int i, j;
+  for(i=0; i < rows_; ++i){
+    for(j=0; j < cols_; ++j){
+      if (j != 0) { std::cout << " "; }
+      std::cout << std::setw(5) << std::setfill(' ')
+                << static_cast<float>(data_[i*cols_padding + j]);
+    }
+    std::cout << std::endl;
+  }
+}
+
+template <>
 void Matrix<float>::printMatrixWithPad()
 {
   int i, j;
@@ -85,11 +104,24 @@ void Matrix<float>::printMatrixWithPad()
   }
 }
 
+template <>
+void Matrix<short>::printMatrixWithPad()
+{
+  int i, j;
+  for(i=0; i < rows_padding; ++i){
+    for(j=0; j < cols_padding; ++j){
+      if (j != 0) { std::cout << " "; }
+      std::cout << std::setw(5) << std::setfill(' ') 
+                << static_cast<float>(data_[i*cols_padding + j]);
+    }
+    std::cout << std::endl;
+  }
+}
 
 /*
   Partial Mult Helper Function - Does 8x8 dimension float multiplication
 */
-Matrix<float>& multHelper_8x(const Matrix<float>& a, int i_A, int j_A,
+void multHelper_8x8(const Matrix<float>& a, int i_A, int j_A,
                              const Matrix<float>& b, int i_B, int j_B,
                              Matrix<float>& c)
 {
@@ -117,12 +149,10 @@ Matrix<float>& multHelper_8x(const Matrix<float>& a, int i_A, int j_A,
     _mm256_storeu_ps(&c(i_a, j_B), sum_current_row); // all results for the current row of 8x output elements
     sum_current_row = _mm256_setzero_ps(); // reset the sums to 0.0
   }
-
-  return c;
 }
 
 /*
-  Intel AVX Instruction-Based Matrix Multiplication
+  Intel AVX Instruction-Based Matrix Multiplication for float data-type
 */
 Matrix<float> operator*(const Matrix<float>& a, const Matrix<float>& b)
 {
@@ -136,7 +166,64 @@ Matrix<float> operator*(const Matrix<float>& a, const Matrix<float>& b)
   for(int i = 0; i < num_256xBlock; i++) {
     for(int j = 0; j < num_256xBlock; j++) {
       for (int k = 0; k < num_256xBlock; k++) {
-        multHelper_8x(a,i*seg_breakup,k*seg_breakup, b,k*seg_breakup,j*seg_breakup, c);
+        multHelper_8x8(a,i*seg_breakup,k*seg_breakup, b,k*seg_breakup,j*seg_breakup, c);
+      }
+    }
+  }
+
+  return c;
+}
+
+
+/*
+  Partial Mult Helper Function - Does 8x8 dimension short multiplication
+*/
+void multHelper_8x8(const Matrix<short>& a, int i_A, int j_A,
+                             const Matrix<short>& b, int i_B, int j_B,
+                             Matrix<short>& c)
+{
+  int seg_breakup = 128/(sizeof(short)*8);
+  __m128i sum_current_row = _mm_set1_epi16(0);
+  for (int i_a = i_A; i_a < i_A+seg_breakup; i_a++) { 
+    __m128i a_ = _mm_loadu_si16(&a(i_a, j_A)); // Get row of 8x elements from A
+    float a_row[seg_breakup]; // 8x 32bit floats in 256 bits
+    _mm_storeu_si16(a_row, a_); // Breakup row of data into individual elements
+
+    for (int i_b = i_B, a_row_itr=0; i_b < i_B+seg_breakup; i_b++, a_row_itr++) { // Go through rows of B
+      __m128i dup_rVal = _mm_set1_epi16(a_row[a_row_itr]); // duplicate value from row to all values in __m256
+
+      __m128i b_row = _mm_loadu_si16(&b(i_b, j_B)); // load in row of B
+
+      // Multiply every value of B row with the duplicated A row value, 
+      // then add each value to the sums for the row. Each sum value is
+      // the value to be put in the position of the 8x8 row value.
+      sum_current_row = _mm_add_epi16(sum_current_row, _mm_mullo_epi16(dup_rVal, b_row));
+    }
+
+    // Load in the information already at C(i_a, j_B) and add new information to it
+    __m128i temp = _mm_loadu_si16(&c(i_a, j_B)); // get current result at c(i_a,j_A)
+    sum_current_row = _mm_add_epi16(sum_current_row, temp); // add current result to new results
+    _mm_storeu_si16(&c(i_a, j_B), sum_current_row); // all results for the current row of 8x output elements
+    sum_current_row = _mm_set1_epi16(0); // reset the sums to 0.0
+  }
+}
+
+/*
+  Intel AVX Instruction-Based Matrix Multiplication for short data-type
+*/
+Matrix<short> operator*(const Matrix<short>& a, const Matrix<short>& b)
+{
+  Matrix<short> c(a.Rows());  // Assuming square matrix
+
+  int seg_breakup = 128/(sizeof(short)*8);
+  int num_128xBlock = ceil((float)c.Rows()/seg_breakup); // find the number of 128 bit segments
+  
+  // Must use Tiling with 8x8 Matrices
+  // i & j correspond to tile/block of 8x8 matrix locations
+  for(int i = 0; i < num_128xBlock; i++) {
+    for(int j = 0; j < num_128xBlock; j++) {
+      for (int k = 0; k < num_128xBlock; k++) {
+        multHelper_8x8(a,i*seg_breakup,k*seg_breakup, b,k*seg_breakup,j*seg_breakup, c);
       }
     }
   }
@@ -150,61 +237,117 @@ int main(int argc, const char** argv)
   clock_t start, end;
   const char* const exeName = argv[0]; // Name of file to compress
 
-  if (argc != 2) { 
+  if (argc != 3) { 
       printf("Wrong Arguments\n");
-      printf("%s MATRIX_DIM\n", exeName);
+      printf("%s MATRIX_DIM DATA_TYPE\n", exeName);
+      printf("DATA_TYPE = i (short) or f (float)\n");
       return 1;
   }
 
   // Square Matrix Dimensions
   int matrix_dim = atoi(argv[1]);
+  std::string argv2 = argv[2];
 
-  // Setup of Matrix A and Matrix B
-  // Values calculated with random float function
-
-  int i, j;
-  Matrix<float> A = Matrix<float>(matrix_dim);
-  for( i = 0; i < matrix_dim; ++i) {
-    for( j = 0;  j < matrix_dim; ++j) {
-      A.setVal(i, j, rand() / (RAND_MAX + 1.)); // float between 0 and 1
+  if(argv2 == "f")
+  {
+    // Setup of Matrix A and Matrix B
+    // Values calculated with random float function
+    int i, j;
+    Matrix<float> A = Matrix<float>(matrix_dim);
+    for( i = 0; i < matrix_dim; ++i) {
+      for( j = 0;  j < matrix_dim; ++j) {
+        A.setVal(i, j, rand() / (RAND_MAX + 1.)); // float between 0 and 1
+      }
     }
-  }
-  Matrix<float> B = Matrix<float>(matrix_dim);
-  for( i = 0; i < matrix_dim; ++i) {
-    for( j = 0;  j < matrix_dim; ++j) {
-      B.setVal(i, j, rand() / (RAND_MAX + 1.)); // float between 0 and 1
+    Matrix<float> B = Matrix<float>(matrix_dim);
+    for( i = 0; i < matrix_dim; ++i) {
+      for( j = 0;  j < matrix_dim; ++j) {
+        B.setVal(i, j, rand() / (RAND_MAX + 1.)); // float between 0 and 1
+      }
     }
+
+    // Print out initial matrices (if dim < 20)
+    if(matrix_dim <= 20)
+    {
+      A.printMatrix();
+      std::cout << std::endl;
+      B.printMatrix();
+      std::cout << std::endl;
+    }
+
+    // Timer Start
+    start = clock();
+
+    // Matrix multiplication
+    Matrix<float> C = A * B;
+
+    // Timer End
+    end = clock();
+
+    // Print out final multiplied matrix
+    if(matrix_dim <= 20) {
+      C.printMatrix();
+      std::cout << std::endl;
+    }
+
+    // Calculating total time taken by the program.
+    double time_taken = double(end - start) / CLOCKS_PER_SEC;
+    std::cout << "Time taken by SIMD matrix multiplication is : " << std::fixed
+          << time_taken << std::setprecision(5);
+    std::cout << " sec " << std::endl;
   }
 
-  // Print out initial matrices (if dim < 20)
-  // if(matrix_dim <= 20)
-  // {
-  //   A.printMatrix();
-  //   std::cout << std::endl;
-  //   B.printMatrix();
-  //   std::cout << std::endl;
-  // }
+  else if(argv2 == "i")
+  {
+    // Setup of Matrix A and Matrix B
+    // Values calculated with random float function
+    int i, j;
+    Matrix<short> A = Matrix<short>(matrix_dim); //value cap of a short is 65535
+    for( i = 0; i < matrix_dim; ++i) {
+      for( j = 0;  j < matrix_dim; ++j) {
+        A.setVal(i, j, (short)(rand() % 10)); // float between 0 and 9
+      }
+    }
+    Matrix<short> B = Matrix<short>(matrix_dim);
+    for( i = 0; i < matrix_dim; ++i) {
+      for( j = 0;  j < matrix_dim; ++j) {
+        B.setVal(i, j, (short)(rand() % 10)); // float between 0 and 9
+      }
+    }
 
-  // Timer Start
-  start = clock();
+    // Print out initial matrices (if dim < 20)
+    if(matrix_dim <= 20)
+    {
+      A.printMatrix();
+      std::cout << std::endl;
+      B.printMatrix();
+      std::cout << std::endl;
+    }
 
-  // Matrix multiplication
-  Matrix<float> C = A * B;
+    // Timer Start
+    start = clock();
 
-  // Timer End
-  end = clock();
+    // Matrix multiplication
+    Matrix<short> C = A * B;
 
-  // Print out final multiplied matrix
-  if(matrix_dim <= 20) {
-    C.printMatrix();
-    std::cout << std::endl;
+    // Timer End
+    end = clock();
+
+    // Print out final multiplied matrix
+    if(matrix_dim <= 20) {
+      C.printMatrix();
+      std::cout << std::endl;
+    }
+
+    // Calculating total time taken by the program.
+    double time_taken = double(end - start) / CLOCKS_PER_SEC;
+    std::cout << "Time taken by SIMD matrix multiplication is : " << std::fixed
+          << time_taken << std::setprecision(5);
+    std::cout << " sec " << std::endl;
   }
-
-  // Calculating total time taken by the program.
-  double time_taken = double(end - start) / CLOCKS_PER_SEC;
-  std::cout << "Time taken by SIMD matrix multiplication is : " << std::fixed
-        << time_taken << std::setprecision(5);
-  std::cout << " sec " << std::endl;
+  else{
+    std::cout << "DATA_TYPE Input is Invalid - Try 'i' or 'f'" << std::endl;
+  }
 
   return 0;
 }
