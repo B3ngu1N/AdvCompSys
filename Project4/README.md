cfacfe642790c857fb4ccc3a2ed68c23249de491
Tom Petr <petrt@rpi.edu>
2023-03-07 17:15:43 -0500

    updates to readme and p4

--- Project4/README.md
+++ Project4/README.md
@@ -1,5 +1,7 @@
 # Project 4: Implementation of Dictionary Codec
 
+Multithreading for data insertion/encoding:
 
+Data from input will be segmented, encoded into separate trees, and then those separate trees will be merged.
 
 
---
+++ Project4/btree_dict.cpp
@@ -0,0 +1,24 @@
+/*
+    Dictionary implementation using a B-Tree structure
+
+    @author Thomas Petr
+    @author Ben Haft
+*/
+
+#include <iostream>
+#include <string>
+
+
+class BTree_Node
+{
+  public:
+    BTree_Node** child_ptr;
+};
+
+
+
+int main(char argc, char** argv)
+{
+
+    return 0;
+}
